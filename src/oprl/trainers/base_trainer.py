59aa4c9b5495c82b0b15e7dcd7bce239bf32977d
Igor Kuznetsov <igorkuznetsov14@gmail.com>
2023-11-27 00:45:42 +0000

    Running ddpg is done from config entrypoint

--- src/oprl/trainers/base_trainer.py
+++ src/oprl/trainers/base_trainer.py
@@ -187,3 +187,30 @@ class BaseTrainer:
             qs.append(q)
 
         return np.mean(qs)
+
+
+def run_training(make_algo, make_env, make_logger, config, seed):
+    env = make_env(seed=seed)
+    logger = make_logger(seed)
+    print("env created.")
+
+    trainer = BaseTrainer(
+        state_shape=config["state_shape"],
+        action_shape=config["action_shape"],
+        env=env,
+        make_env_test=make_env,
+        algo=make_algo(logger, seed),
+        num_steps=config["num_steps"],
+        eval_interval=config["eval_every"],
+        device=config["device"],
+        save_buffer_every=config["save_buffer"],
+        visualise_every=config["visualise_every"],
+        estimate_q_every=config["estimate_q_every"],
+        stdout_log_every=config["log_every"],
+        seed=seed,
+        logger=logger,
+    )
+    print("Trianer created.")
+
+    trainer.train()
+    print("Training end.")
