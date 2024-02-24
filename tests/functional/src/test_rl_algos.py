9298c23a9e20edf0ec1a30ffd2fbb1b379111535
Igor Kuznetsov <igorkuznetsov14@gmail.com>
2024-02-24 23:08:15 +0000

    Add tests files

---
+++ tests/functional/src/test_rl_algos.py
@@ -0,0 +1,35 @@
+import pytest
+import torch
+
+from oprl.algos.ddpg import DDPG
+from oprl.algos.sac import SAC
+from oprl.algos.td3 import TD3
+from oprl.algos.tqc import TQC
+from oprl.env import DMControlEnv
+
+rl_algo_classes = [DDPG, SAC, TD3, TQC]
+
+
+@pytest.mark.parametrize("algo_class", rl_algo_classes)
+def test_rl_algo_run(algo_class):
+    env = DMControlEnv("walker-walk", seed=0)
+    obs, _ = env.reset(env.sample_action())
+
+    algo = algo_class(
+        state_shape=env.observation_space.shape,
+        action_shape=env.action_space.shape,
+    )
+    action = algo.exploit(obs)
+    assert action.ndim == 1
+
+    action = algo.explore(obs)
+    assert action.ndim == 1
+
+    _batch_size = 8
+    batch_obs = torch.randn(_batch_size, env.observation_space.shape[0])
+    batch_actions = torch.clamp(
+        torch.randn(_batch_size, env.action_space.shape[0]), -1, 1
+    )
+    batch_rewards = torch.randn(_batch_size, 1)
+    batch_dones = torch.randint(2, (_batch_size, 1))
+    algo.update((batch_obs, batch_actions, batch_rewards, batch_dones, batch_obs))
